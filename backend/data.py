40e5c8dda39270b0568c6224320ac3ba79b4d951
Ayush <ayushsh285@gmail.com>
2025-06-03 17:39:34 -0400

    cache based on user input

--- backend/data.py
+++ backend/data.py
@@ -2,17 +2,15 @@ from fastapi import APIRouter, HTTPException, Request
 from rapidfuzz import fuzz
 
 from backend.utils import get_school_id_by_code, get_prof, get_tags_comments
-from backend.models import ProfessorInfo
+from backend.models import ProfessorInfo, Comment, Tag
 
+import json
 
 router = APIRouter()
 
 
 @router.get("/get_professor_info" , response_model=ProfessorInfo)
 async def get_professor_info(request: Request, prof_first_name: str | None = None, prof_last_name: str | None = None, school_code: str | None = None):
-    
-    if not request.app.state.redis_instance:
-        pass
     
     if not school_code:
         raise HTTPException(status_code=400, detail="School code is required")
@@ -22,6 +20,15 @@ async def get_professor_info(request: Request, prof_first_name: str | None = Non
     if not school_id:
         raise HTTPException(status_code=404, detail="School not found")
     
+    redis = None
+    cache_key = f"professor_info:{school_id}:{prof_first_name.lower() if prof_first_name else ''}:{prof_last_name.lower() if prof_last_name else ''}"
+    if request.app.state.redis_instance:
+        redis = request.app.state.redis_instance
+        cached = await redis.get(cache_key)
+        if cached:
+            return ProfessorInfo(**json.loads(cached))
+        
+    # if control is here, cache miss
     try:
         professors = get_prof(prof_first_name, prof_last_name, school_id)
     except Exception as e:
@@ -69,5 +76,7 @@ async def get_professor_info(request: Request, prof_first_name: str | None = Non
     
     # get tags and comments for best match
     best_match.tags, best_match.userCards = get_tags_comments(best_match.legacyId)
-
+    
+    if redis:
+        await redis.set(cache_key, json.dumps(best_match.model_dump()), ex=432000)  # Cache for 5 days
     return best_match
