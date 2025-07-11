4577e81326acf0dba3869d4f1268803f5d38913f
Ayush <ayushsh285@gmail.com>
2025-07-11 15:12:14 -0400

    chrome api methods

---
+++ frontend-react/chrome/rightClickReceiver.js
@@ -0,0 +1,11 @@
+// right click receiver
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    let firstNameInput = document.getElementById("first-name");
+    let lastNameInput = document.getElementById("last-name");
+    firstNameInput.value = request.firstName || "";
+    lastNameInput.value = request.lastName || "";
+    if (request.firstName || request.lastName) {
+        document.getElementById("submit-button").click();
+    }
+});
