add04f32946ed4a7af4711527eed184c8595a385
Ayush <ayushsh285@gmail.com>
2025-07-11 15:06:34 -0400

    delete old frontend folder

--- frontend/rightClickReceiver.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    let firstNameInput = document.getElementById("first-name");
-    let lastNameInput = document.getElementById("last-name");
-    firstNameInput.value = request.firstName || "";
-    lastNameInput.value = request.lastName || "";
-    if (request.firstName || request.lastName) {
-        document.getElementById("submit-button").click();
-    }
-});
