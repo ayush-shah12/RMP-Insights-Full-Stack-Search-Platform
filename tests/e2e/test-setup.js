e9649c0c172aa32e7eb1a598321720c85cf12fed
Ayush <ayushsh285@gmail.com>
2025-07-11 22:58:54 -0400

    test setup file

---
+++ tests/e2e/test-setup.js
@@ -0,0 +1,41 @@
+import { test as base, chromium } from '@playwright/test';
+import { execSync } from 'child_process';
+import path from 'path';
+
+export const test = base.extend({
+  context: async ({ }, use) => {
+    // Build the React extension first
+    const extensionPath = path.join(__dirname, '../../v2-client-react-ts');
+    console.log('Building React extension...');
+    try {
+      execSync('npm run build', { 
+        cwd: extensionPath, 
+        stdio: 'inherit' 
+      });
+    } catch (error) {
+      console.error('Failed to build extension:', error);
+      throw error;
+    }
+    
+    const pathToExtension = path.join(extensionPath, 'build');
+    const context = await chromium.launchPersistentContext('', {
+      headless: false,
+      args: [
+        `--headless=chrome`,
+        `--disable-extensions-except=${pathToExtension}`,
+        `--load-extension=${pathToExtension}`,
+      ],
+    });
+    await use(context);
+    await context.close();
+  },
+  extensionId: async ({ context }, use) => {
+    let [background] = context.serviceWorkers();
+    if (!background)
+      background = await context.waitForEvent('serviceworker');
+    const extensionId = background.url().split('/')[2];
+    await use(extensionId);
+  },
+});
+export const expect = test.expect;
+	
